db1a2219b56b6ec3548f938f88960408c65f3375
Rohit Dalal <dalalrohit102@gmail.com>
2019-04-05 16:13:35 +0530

    Updates

---
+++ precache-manifest.bd2e2a0c58ddc5bdd2b6deaee81914a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57de28ec6c48c5277a2f",
+    "url": "/react-weatherapp/static/js/runtime~main.503af55b.js"
+  },
+  {
+    "revision": "4d32141bf707731f5dbd",
+    "url": "/react-weatherapp/static/js/main.85657ad1.chunk.js"
+  },
+  {
+    "revision": "a0ea9715bbc32a6e2e5a",
+    "url": "/react-weatherapp/static/js/2.bf9b2f5b.chunk.js"
+  },
+  {
+    "revision": "4d32141bf707731f5dbd",
+    "url": "/react-weatherapp/static/css/main.c0aeab54.chunk.css"
+  },
+  {
+    "revision": "42916d0d804cac7cf796a7053a81bf68",
+    "url": "/react-weatherapp/index.html"
+  }
+];
